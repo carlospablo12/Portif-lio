93405218d0fd8dbe27dfda252188b1431146b9e2
Carlos Pablo <carlospablo796@gmail.com>
2025-08-05 23:30:43 -0300

    Atualizações e refatoração

---
+++ revel.js
@@ -0,0 +1,22 @@
+// Fazendo o resete da página
+window.revelar = ScrollReveal({ reset: true });
+
+// TOPO DO SITE
+revelar.reveal(".effect-img-header", {
+  duration: 2000,
+  distance: "90px",
+});
+
+revelar.reveal(".effect-txt-header", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+});
+revelar.reveal(".effect-txt-tecnologia", {
+  duration: 2000,
+  distance: "90px",
+});
+revelar.reveal(".effect-txt-projeto", {
+  duration: 2000,
+  distance: "90px",
+});
